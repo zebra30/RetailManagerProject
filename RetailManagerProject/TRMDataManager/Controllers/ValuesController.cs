e0d48feb61073afa79873ea4acc23d80621879f0
Alex Klo <aaom@nni.net>
2019-12-13 18:28:45 -0500

    Add Swagger

--- RetailManagerProject/TRMDataManager/Controllers/ValuesController.cs
+++ RetailManagerProject/TRMDataManager/Controllers/ValuesController.cs
@@ -12,11 +12,11 @@ namespace TRMDataManager.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IHttpActionResult Get()
+        public IEnumerable<string> Get()
         {
             var userId = RequestContext.Principal.Identity.GetUserId();
 
-            return Ok(new string[] { "value1", "value2", userId });
+            return new string[] { "value1", "value2", userId };
         }
 
         // GET api/values/5
